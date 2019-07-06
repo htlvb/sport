96d82f4707994e605bcd4a5ef012c6bc95b4624b
Johannes Egger <j.egger@posteo.at>
2019-07-06 21:56:10 +0200

    Add sample client to grant app permissions

---
+++ GrantAuthSample/Program.fs
@@ -0,0 +1,18 @@
+﻿open Microsoft.Identity.Client
+open System
+
+[<EntryPoint>]
+let main argv =
+    let clientApp =
+        PublicClientApplicationBuilder.Create(Environment.GetEnvironmentVariable "APP_ID")
+            .WithAuthority(AzureCloudInstance.AzurePublic, Environment.GetEnvironmentVariable "TENANT_ID")
+            .Build()
+    let authResult =
+        clientApp
+            .AcquireTokenInteractive([| "Files.ReadWrite" |])
+            .ExecuteAsync()
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    printfn "Auth token: %s" authResult.AccessToken
+    0
